9371c6aeac58608c169cd4fe49c985e1b236600b
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-25 23:43:09 +0000

    getAllProperties to use property listings query

--- LightBnB_WebApp-master 2/server/database.js
+++ LightBnB_WebApp-master 2/server/database.js
@@ -119,12 +119,52 @@ exports.getAllReservations = getAllReservations;
 // }
 
 const getAllProperties = (options, limit = 10) => {
+  
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, AVG(property_reviews.rating) AS average_rating
+  FROM properties
+  LEFT JOIN property_reviews ON property_id = properties.id
+  `
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+  if (options.owner_id) {
+    queryParams.length > 0 ? queryString += `AND ` : queryString += `WHERE `;
+    queryParams.push(Number(options.owner_id));
+    queryString += `owner_id = $${queryParams.length} `;
+  }
+  if (options.minimum_price_per_night) {
+    queryParams.length > 0 ? queryString += `AND ` : queryString += `WHERE `;
+    queryParams.push(`${options.minimum_price_per_night}`);
+    queryString += `cost_per_night >= $${queryParams.length} `;
+  }
+  if (options.maximum_price_per_night) {
+    queryParams.length > 0 ? queryString += `AND ` : queryString += `WHERE `;
+    queryParams.push(`${options.maximum_price_per_night}`);
+    queryString += `cost_per_night <= $${queryParams.length} `;
+  }
+  if (options.minimum_rating) {
+    queryParams.length > 0 ? queryString += `AND ` : queryString += `WHERE `;
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `rating >= $${queryParams.length} ` 
+  }
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `
+  
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => result.rows)
-    .catch((err) => {
-      console.log(err.message);
-    });
+  .query(queryString, queryParams)
+  .then(res => res.rows)
+  .catch((err) => {
+    console.error('query error', err.stack);
+  });
+
+
 };
 exports.getAllProperties = getAllProperties;
 
